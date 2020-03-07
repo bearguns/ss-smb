1a538a4a2b6e65168aa8e8eda4b326dc4f8fbb8d
Sean Brage <seanmbrage@me.com>
2020-03-07 13:05:55 -0700

    update make-pdf script

--- make-pdf.js
+++ make-pdf.js
@@ -9,7 +9,7 @@ async function main() {
     })
     const page = await browser.newPage()
     await page.goto('http://localhost:3000/pdf', { waitUntil: 'networkidle2' })
-    await page.pdf({ path: 'public/resume.pdf' })
+    await page.pdf({ path: 'static/resume.pdf' })
 
     await browser.close()
   } catch (exception) {
